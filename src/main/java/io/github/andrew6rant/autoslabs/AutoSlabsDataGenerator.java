26ed3efedb2ab4b5ed1afb016474142b03343c5d
Andrew Grant <andrew.grant@mail.rit.edu>
2023-01-08 18:03:33 -0500

    properly integrate modid

--- src/main/java/io/github/andrew6rant/autoslabs/AutoSlabsDataGenerator.java
+++ src/main/java/io/github/andrew6rant/autoslabs/AutoSlabsDataGenerator.java
@@ -3,7 +3,7 @@ package io.github.andrew6rant.autoslabs;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
-public class ExampleModDataGenerator implements DataGeneratorEntrypoint {
+public class AutoSlabsDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 
