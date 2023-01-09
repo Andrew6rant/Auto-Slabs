daeced816875a8a61da5f66e2a0488b33e62790f
Andrew Grant <andrew.grant@mail.rit.edu>
2023-01-09 00:32:22 -0500

    properly set up ARRP generation

--- src/main/java/io/github/andrew6rant/autoslabs/AutoSlabs.java
+++ src/main/java/io/github/andrew6rant/autoslabs/AutoSlabs.java
@@ -19,26 +19,25 @@ import virtuoel.statement.api.StateRefresher;
 
 public class AutoSlabs implements ModInitializer {
 	public static final RuntimeResourcePack AUTO_SLABS_RESOURCES = RuntimeResourcePack.create("autoslabs:resources");
-	public static final String MOD_ID = "autoslabs:";
 
 	@Override
 	public void onInitialize() {
 		for (Block block : Registries.BLOCK) {
 			if (block instanceof SlabBlock) {
+				Identifier id = Registries.BLOCK.getId(block);
+				String namespace = id.getNamespace();
+				String path = id.getPath();
 				StateRefresher.INSTANCE.addBlockProperty(block, EnumProperty.of("vertical_type", VerticalType.class), VerticalType.FALSE);
-				System.out.println("Added vertical_type property to " + block.getRegistryEntry()+", "+block.getLootTableId()+", "+block.getLootTableId().getPath());
-				//AUTO_SLABS_RESOURCES.addModel(JModel.model("autoslabs:block/template_bottom_vertical").textures(JModel.textures().var("texture", block.getTranslationKey())), new Identifier(block.getLootTableId()+"_bottom_vertical"));
-				//AUTO_SLABS_RESOURCES.addModel(JModel.model("autoslabs:block/template_top_vertical").textures(JModel.textures().var("texture", block.getTranslationKey())), new Identifier(block.getLootTableId()+"_top_vertical"));
 				AUTO_SLABS_RESOURCES.addBlockState(JState.state(new JVariant()
-						.put("type=bottom,vertical_type=north_south", JState.model(block.getLootTableId()).x(90))
-						.put("type=bottom,vertical_type=east_west", JState.model(block.getLootTableId()).x(90).y(90))
-						.put("type=top,vertical_type=north_south", JState.model(block.getLootTableId()+"_top").x(90))
-						.put("type=top,vertical_type=east_west", JState.model(block.getLootTableId()+"_top").x(90).y(90))
-				), block.getLootTableId());
-
+						.put("type=bottom,vertical_type=north_south", JState.model(namespace+":block/"+path).x(90))
+						.put("type=bottom,vertical_type=east_west", JState.model(namespace+":block/"+path).x(90).y(90))
+						.put("type=top,vertical_type=north_south", JState.model(namespace+":block/"+path+"_top").x(90))
+						.put("type=top,vertical_type=east_west", JState.model(namespace+":block/"+path+"_top").x(90).y(90))
+				), id);
 			}
 		}
 		StateRefresher.INSTANCE.reorderBlockStates();
-		RRPCallback.BEFORE_VANILLA.register(a -> a.add(AUTO_SLABS_RESOURCES));
+		RRPCallback.AFTER_VANILLA.register(a -> a.add(AUTO_SLABS_RESOURCES));
+		//AUTO_SLABS_RESOURCES.dump();
 	}
 }
