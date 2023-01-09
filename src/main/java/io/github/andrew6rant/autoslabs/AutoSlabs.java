a55bb4bbc55f269a7a3a05c879a0a5bef98976a2
Andrew Grant <andrew.grant@mail.rit.edu>
2023-01-08 22:51:51 -0500

    initial ARRP integration
    not everything is working yet

--- src/main/java/io/github/andrew6rant/autoslabs/AutoSlabs.java
+++ src/main/java/io/github/andrew6rant/autoslabs/AutoSlabs.java
@@ -1,5 +1,10 @@
 package io.github.andrew6rant.autoslabs;
 
+import net.devtech.arrp.api.RRPCallback;
+import net.devtech.arrp.api.RuntimeResourcePack;
+import net.devtech.arrp.json.blockstate.JState;
+import net.devtech.arrp.json.blockstate.JVariant;
+import net.devtech.arrp.json.models.JModel;
 import net.fabricmc.api.ModInitializer;
 
 import net.minecraft.block.Block;
@@ -7,23 +12,33 @@ import net.minecraft.block.Blocks;
 import net.minecraft.block.SlabBlock;
 import net.minecraft.registry.Registries;
 import net.minecraft.state.property.EnumProperty;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import virtuoel.statement.api.StateRefresher;
 
 public class AutoSlabs implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("autoslabs");
+	public static final RuntimeResourcePack AUTO_SLABS_RESOURCES = RuntimeResourcePack.create("autoslabs:resources");
+	public static final String MOD_ID = "autoslabs:";
 
 	@Override
 	public void onInitialize() {
 		for (Block block : Registries.BLOCK) {
 			if (block instanceof SlabBlock) {
 				StateRefresher.INSTANCE.addBlockProperty(block, EnumProperty.of("vertical_type", VerticalType.class), VerticalType.FALSE);
+				System.out.println("Added vertical_type property to " + block+", "+block.getLootTableId()+", "+block.getTranslationKey()+", "+block.getLootTableId().getPath());
+				AUTO_SLABS_RESOURCES.addModel(JModel.model("autoslabs:block/template_bottom_vertical").textures(JModel.textures().var("texture", block.getTranslationKey())), new Identifier(block.getLootTableId()+"_bottom_vertical"));
+				AUTO_SLABS_RESOURCES.addModel(JModel.model("autoslabs:block/template_top_vertical").textures(JModel.textures().var("texture", block.getTranslationKey())), new Identifier(block.getLootTableId()+"_top_vertical"));
+				AUTO_SLABS_RESOURCES.addBlockState(JState.state(new JVariant()
+						.put("type=bottom,vertical_type=north_south", JState.model(MOD_ID+block.getLootTableId().getPath()+"_bottom_vertical"))
+						.put("type=bottom,vertical_type=east_west", JState.model(MOD_ID+block.getLootTableId().getPath()+"_bottom_vertical").y(90))
+						.put("type=top,vertical_type=north_south", JState.model(MOD_ID+block.getLootTableId().getPath()+"_top_vertical"))
+						.put("type=top,vertical_type=east_west", JState.model(MOD_ID+block.getLootTableId().getPath()+"_top_vertical").y(90))
+				), block.getLootTableId());
+
 			}
 		}
 		StateRefresher.INSTANCE.reorderBlockStates();
+		RRPCallback.EVENT.register(a -> a.add(AUTO_SLABS_RESOURCES));
 	}
 }
