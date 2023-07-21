6ec3178ee489adff1e46cf1309ae02d2f72c711f
アカシイ - Akashii <44606942+AkashiiKun@users.noreply.github.com>
2023-07-21 01:16:10 +0100

    Added Built-In Resource Pack.

--- src/main/java/io/github/andrew6rant/autoslabs/AutoSlabsClient.java
+++ src/main/java/io/github/andrew6rant/autoslabs/AutoSlabsClient.java
@@ -3,8 +3,11 @@ package io.github.andrew6rant.autoslabs;
 import net.fabricmc.api.ClientModInitializer;
 
 public class AutoSlabsClient implements ClientModInitializer {
+	final ModContainer container = FabricLoader.getInstance().getModContainer("autoslabs").get();
+
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		ResourceManagerHelper.registerBuiltinResourcePack(new Identifier("autoslab", "distinct_slabs"), container, Text.literal("Distinct Slabs (Built-In)"), ResourcePackActivationType.NORMAL);
 	}
 }
