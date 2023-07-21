882668fafd5215897727cd9128489d83ec3954a6
Andrew Grant <andrew.grant@mail.rit.edu>
2023-07-20 22:08:46 -0400

    import packages to properly compile changes

--- src/main/java/io/github/andrew6rant/autoslabs/AutoSlabsClient.java
+++ src/main/java/io/github/andrew6rant/autoslabs/AutoSlabsClient.java
@@ -1,6 +1,12 @@
 package io.github.andrew6rant.autoslabs;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.ModContainer;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 
 public class AutoSlabsClient implements ClientModInitializer {
 	final ModContainer container = FabricLoader.getInstance().getModContainer("autoslabs").get();
