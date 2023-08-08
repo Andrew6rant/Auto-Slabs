ffc409f10f929f20a9aff864b49b8228edfb470a
Andrew Grant <andrew.grant@mail.rit.edu>
2023-08-08 17:12:21 -0400

    support Statement API 4.2.6+
    Statement API changed from using `org.apache.logging.log4j.Logger` to `org.spongepowered.asm.logging.ILogger`. I can no longer change the level of the logger programmatically, so I have to use a different method (making a mixin that runs before theirs)

--- src/main/java/io/github/andrew6rant/autoslabs/mixin/StateMixin.java
+++ src/main/java/io/github/andrew6rant/autoslabs/mixin/StateMixin.java
@@ -2,26 +2,45 @@ package io.github.andrew6rant.autoslabs.mixin;
 
 import io.github.andrew6rant.autoslabs.config.ServerConfig;
 import net.minecraft.state.State;
-import org.apache.logging.log4j.Level;
-import org.apache.logging.log4j.core.config.Configurator;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
+import net.minecraft.state.property.Property;
+import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import virtuoel.statement.Statement;
-import virtuoel.statement.api.StatementApi;
+import virtuoel.statement.util.StatementStateExtensions;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Mixin(State.class)
-public class StateMixin<O, S> {
+public class StateMixin<O, S> implements StatementStateExtensions<S> {
+
+    @Unique String getMissingOwner = "";
+    @Shadow @Final @Mutable protected O owner; // cachedFallbacks and owner are modified in Statement API's StateMixin
+    @Unique final Map<Property<?>, Comparable<?>> cachedFallbacks = new HashMap<>();
+
+    @Inject(method = "get", cancellable = true, at = @At(value = "INVOKE", target = "Ljava/lang/IllegalArgumentException;<init>(Ljava/lang/String;)V", shift = At.Shift.BEFORE))
+    private <T extends Comparable<T>> void autoslabs$onGet(Property<T> property, CallbackInfoReturnable<T> info) {
+        final String ownerString = this.owner.toString();
+
+        if (!getMissingOwner.equals(ownerString)) {
+            if (!ServerConfig.suppressStatementAPILogger) {
+                Statement.LOGGER.info("Cannot get property {} as it does not exist in {}", property, this.owner);
+            }
+            getMissingOwner = ownerString;
+        }
+
+        info.setReturnValue(cachedFallbacks.containsKey(property) ? property.getType().cast(cachedFallbacks.get(property)) : property.getValues().iterator().next());
+    }
 
     static {
         // I need to initialize the config earlier than onInitialize() runs in order to suppress Statement API's logger based on a config value
         ServerConfig.init("auto_slabs", ServerConfig.class);
         if (ServerConfig.suppressStatementAPILogger) {
-            // Immense amount of trolling
-            Statement.LOGGER.error("Statement API's logging has been disabled by AutoSlabs!");
-            Configurator.setLevel(StatementApi.MOD_ID, Level.FATAL);
-        }
-        else {
-            Statement.LOGGER.error("AutoSlabs: don't worry about these errors :)");
+            Statement.LOGGER.warn("Statement API's logging has been disabled by AutoSlabs!");
+        } else {
+            Statement.LOGGER.warn("AutoSlabs: don't worry about these errors :)");
         }
     }
 
